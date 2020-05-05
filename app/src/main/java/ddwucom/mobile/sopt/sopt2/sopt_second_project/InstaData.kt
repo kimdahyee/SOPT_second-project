acf3716fba79bd42a3c166e78f8178332a1d12f2
kimdahyee <diana6777@naver.com>
2020-05-05 20:58:41 +0900

    first commit of SOPT_second-project

---
+++ app/src/main/java/ddwucom/mobile/sopt/sopt2/sopt_second_project/InstaData.kt
@@ -0,0 +1,7 @@
+package ddwucom.mobile.sopt.sopt2.sopt_second_project
+
+data class InstaData (
+    val userName : String,
+    val img_profile : String,
+    val img_content : String
+)
---
+++ app/src/main/java/ddwucom/mobile/sopt/sopt2/sopt_second_project/InstaItemDecoration.kt
@@ -0,0 +1,4 @@
+package ddwucom.mobile.sopt.sopt2.sopt_second_project
+
+class InstaItemDecoration {
+}
